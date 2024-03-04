b2a95db6b4288c70626cdc1014afb7dc4f22a703
Michael <clishamj@miamioh.edu>
2024-03-04 15:10:26 -0500

    fixed

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,4 @@
 // '&' means reference to the value, ' means lifetime of the value (not the reference), static means the lifetime of the program (the entire time the program is running)
-pub const BUFFER_STRING: &'static str =
-    "\n<=====================================================>\n\n";
-pub const ERROR_STRING: &'static str =
-    "\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\n";
-pub const BOARD_HORIZONTAL_LINE: &'static str = "-----------";
+pub const BUFFER_STRING: &str = "\n<=====================================================>\n\n";
+pub const ERROR_STRING: &str = "\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\n";
+pub const BOARD_HORIZONTAL_LINE: &str = "-----------";
